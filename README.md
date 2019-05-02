7bd6dcdde25af49a23b79c973d31d84ca36b7116
Mikhail Popov <mpopov@wikimedia.org>
2019-05-02 10:42:57 -0400

    Final draft

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Daily report of Suggested Edits v1 release on Android
+
+On **notebook1004.eqiad.wmnet** `mkdir /srv/published-datasets/wikipedia-android-app-reports` if one does not already exist.
+
+Then `crontab -e`:
 
 ```
 0 5 * * * bash /home/bearloga/android/suggested_edits/update_publish_notebook.sh
--- update_publish_notebook.sh
+++ update_publish_notebook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
 cd /home/bearloga/android/suggested_edits
 {
+  date
   /home/bearloga/venv/bin/jupyter nbconvert --ExecutePreprocessor.timeout=600 --execute --to html suggested-edits-v1.ipynb
   cp suggested-edits-v1.html /srv/published-datasets/wikipedia-android-app-reports
-} >> notebook_update.log 2>&1
+} >> /home/bearloga/android/suggested_edits/notebook_update.log 2>&1
