bd5569fd1717aa63a51571c6a2dd4d4d9a8b8969
Mikhail Popov <mpopov@wikimedia.org>
2019-05-10 09:06:31 -0400

    Track revert rate

--- update_publish_notebook.sh
+++ update_publish_notebook.sh
@@ -2,6 +2,7 @@
 cd /home/bearloga/android/suggested_edits
 {
   date
-  /home/bearloga/venv/bin/jupyter nbconvert --ExecutePreprocessor.timeout=600 --execute --to html suggested-edits-v1.ipynb
+  Rscript revert_rate.R
+  /home/bearloga/venv/bin/jupyter nbconvert --ExecutePreprocessor.timeout=900 --execute --to html suggested-edits-v1.ipynb
   cp suggested-edits-v1.html /srv/published-datasets/wikipedia-android-app-reports
 } >> /home/bearloga/android/suggested_edits/notebook_update.log 2>&1
